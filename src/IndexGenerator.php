c783b9cfc03c0f16454ca49258b0e47936d60e49
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2021-12-07 14:06:13 +0100

    Update index on saving content

--- src/IndexGenerator.php
+++ src/IndexGenerator.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Bolt\TntSearch;
 
+use Bolt\Entity\Content;
+use Doctrine\ORM\EntityManagerInterface;
+
 class IndexGenerator
 {
     /** @var TntEngine */
@@ -12,18 +15,56 @@ class IndexGenerator
     /** @var \Bolt\Configuration\Config */
     private $boltConfig;
 
-    public function __construct(TntEngine $engine, \Bolt\Configuration\Config $boltConfig)
+    /** @var EntityManagerInterface */
+    private $em;
+
+    public function __construct(TntEngine $engine, \Bolt\Configuration\Config $boltConfig, EntityManagerInterface $em)
     {
         $this->engine = $engine;
         $this->boltConfig = $boltConfig;
+        $this->em = $em;
     }
 
     public function generate(): void
     {
         $tnt = $this->engine->get();
+        $index = $tnt->createIndex('records.index');
+        $index->disableOutput = true;
 
-        $indexer = $tnt->createIndex('records.index');
+        $index->query($this->getQuery());
+        $index->disableOutput = true;
 
+        // todo: makes the process a lot slower, but goes through all of them. Otherwise, the last page
+        // with less items than the step size gets ommitted.
+        $index->steps = 1;
+
+        $index->run();
+    }
+
+    public function update(Content $content): void
+    {
+        $query = $this->getQuery();
+        $whereClause = sprintf(' bolt_content.id = %d AND', $content->getId());
+        $position = mb_strpos($query, 'WHERE') + 5;
+
+        $query = substr_replace($query, $whereClause, $position, 0);
+        $statement = $this->em->getConnection()->prepare($query);
+        $statement->executeStatement();
+        $results = $statement->fetchAll();
+
+        $tnt = $this->engine->get();
+        $tnt->selectIndex('records.index');
+        $index = $tnt->getIndex();
+
+        // Delete the current document, then add all new fields.
+        $index->delete($content->getId());
+        foreach ($results as $result) {
+            $index->insert($result);
+        }
+    }
+
+    protected function getQuery(): string
+    {
         $contentTypes = $this->boltConfig->get('contenttypes')
             ->where('searchable', true)
             ->keys()
@@ -39,13 +80,6 @@ class IndexGenerator
                 WHERE bolt_content.content_type IN (%s);
 EOD;
 
-        $indexer->query(sprintf($query, $contentTypes));
-        $indexer->disableOutput = true;
-
-        // todo: makes the process a lot slower, but goes through all of them. Otherwise, the last page
-        // with less items than the step size gets ommitted.
-        $indexer->steps = 1;
-
-        $indexer->run();
+        return sprintf($query, $contentTypes);
     }
 }
